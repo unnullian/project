7b182ade6f62242a25714543f6693ada9c00776b
Kwakminwoo <30494744+unnullian@users.noreply.github.com>
2018-02-10 11:44:20 +0900

    ddd
    dd

---
+++ arduino/스마트현관프로젝트/WiFiClient/WiFiClient.ino
@@ -0,0 +1,125 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "2.4G";
+const char* password = "01093106215";
+
+const char* host = "www.weather.go.kr";
+const char* streamId   = "....................";
+const char* privateKey = "....................";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+  WiFiClient client;
+  String a[3];
+  int i=0;
+  String temp;
+  String wfEn;
+  String reh;
+  String tmp_str;
+  delay(5000);
+  ++value;
+  // 만약 인체감지가 된다면 api를 불러온다.
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/wid/queryDFSRSS.jsp?zone=1117053000";
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+
+    delay(10);
+    while(client.available()){
+      String line = client.readStringUntil('\n');
+
+      i= line.indexOf("</temp>");
+
+      if(i>0){
+        tmp_str="<temp>";
+        temp = line.substring(line.indexOf(tmp_str)+tmp_str.length(),i);
+        Serial.println(temp); 
+
+      }
+
+      i= line.indexOf("</wfKor>");
+
+      if(i>0){
+        tmp_str="<wfKor>";
+        wfEn = line.substring(line.indexOf(tmp_str)+tmp_str.length(),i);
+        Serial.println(wfEn);  
+      }
+
+      i= line.indexOf("</reh>");
+
+      if(i>0){
+        tmp_str="<reh>";
+        reh = line.substring(line.indexOf(tmp_str)+tmp_str.length(),i);
+        Serial.println(reh);  
+        break;
+      }
+    }
+    
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+}
+
